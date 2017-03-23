0afc2b02317d62e1d71177c5517434739918cf03
Roland Tanglao <roland@rolandtanglao.com>
2017-03-22 23:37:27 -0700

    better error messages to stderr

--- print-domain.rb
+++ print-domain.rb
@@ -14,7 +14,7 @@ Ccsv.foreach(ARGV[0]) do |values|
   field2 = values[2]
   $stderr.printf("2nd record:%s\n",field2) # values[2] to get the URI
   if field2.index("https://") != 0 && field2.index("http://") != 0
-    $stderr.puts("SKIPPING non HTTP and non HTTP record2^^^")
+    $stderr.printf("SKIPPING non HTTP and non HTTP FIELD2:%s; FIELD3:%s\n", field2, values[3])
     next
   end
   uri = URI.parse(values[2])
@@ -22,6 +22,6 @@ Ccsv.foreach(ARGV[0]) do |values|
     domain = PublicSuffix.parse(uri.host)
     puts domain.domain
   rescue PublicSuffix::DomainNotAllowed
-    $stderr.puts("PublicSuffix::DomainNotAllowed^^^")
+    $stderr.printf("PublicSuffix::DomainNotAllowed^^^ URI:%s\n", uri.host)
   end
 end
